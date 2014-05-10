6d22aef60227e4cb4129d0c40521cb9464e44d69
Jakob Odersky <jodersky@gmail.com>
2014-05-10 18:31:53 +0200

    add dummy points

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,11 @@
 /* Controllers */
 var p = new Point(1,2);
 
-app.controller('pointCtrl', function($scope){
-    $scope.song= [
-    ];
-});
+app.controller('pointCtrl', ['$scope', '$http', function($scope, $http){
+	$scope.points=dummyPoints;
+
+	/*$http.get('data.json').then( function (response) {
+		console.log(response.data.points);
+		$scope.points=JSON.parse(response.data.points);
+    });*/
+}]);
