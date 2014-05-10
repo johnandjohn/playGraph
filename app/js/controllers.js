469ba240d6a3e89888a171fb544bbe1fde9275d7
Nicolas Bornand <nicolas74@gmail.com>
2014-05-10 18:50:09 +0200

    add path service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,12 +3,12 @@
 /* Controllers */
 var p = new Point(1,2);
 
-app.controller('pointCtrl', ['$scope', '$http', function($scope, $http){
+app.controller('pointCtrl', function($scope, $http, pathService){
 	var constList = [
 		{x: 10, y:20},
 		{x: 100, y:50},
 		{x: 100, y:200}
-	]
+	];
 	pathService.computePlaylist(dummyPoints,constList, 60);
 	$scope.points=dummyPoints;
 
@@ -16,4 +16,4 @@ app.controller('pointCtrl', ['$scope', '$http', function($scope, $http){
 		console.log(response.data.points);
 		$scope.points=JSON.parse(response.data.points);
     });*/
-}]);
+});
