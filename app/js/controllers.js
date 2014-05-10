9c6e8430f2eae3413ec7d858b5920d70ba14db32
Jakob Odersky <jodersky@gmail.com>
2014-05-11 01:50:09 +0200

    wip - paperjs directive

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -30,111 +30,14 @@ app.controller('pointCtrl', function($scope, $http, pathService){
 		}
 	}
 
+	$scope.foo = function (event) {
+		console.log("foobar");
 
-	// Get a reference to the canvas object
-	var canvas = document.getElementById('canvas');
-	// Create an empty project and a view for the canvas:
-	paper.setup(canvas);
-	// Create a Paper.js Path to draw a line into it:
-	var path = new paper.Path();
-
-
-	// Give the stroke a color
-	path.strokeColor = 'black';
-	var start = new paper.Point(100, 100);
-	var textItem = new paper.PointText({
-		content: 'Click and drag to draw a line.',
-		point: new Point(20, 30),
-		fillColor: 'black',
-	});
-
-	// Move to start and draw a line from there
-	path.moveTo(start);
-	// Note that the plus operator on Point objects does not work
-	// in JavaScript. Instead, we need to call the add() function:
-	path.lineTo(start.add([ 200, -50 ]));
-	// Draw the view now:
-	paper.view.draw();
-
-
-	$scope.mouseDown = function(event){
-		console.log(event);
-    	if (path) {
-			path.selected = false;
-		}
-
-		// Create a new path and set its stroke color to black:
-		path = new paper.Path({
-			segments: [new paper.Point(event.x, event.y)],
-			strokeColor: 'black',
-			// Select the path, so we can see its segment points:
-			fullySelected: true
+		$scope.points.push({
+			x: 250,
+			y: 250
 		});
-    };
-
- $scope.mouseDrag = function(event){
-        path.add(new paper.Point(event.x, event.y));
-
-		// Update the content of the text item to show how many
-		// segments it has:
-		textItem.content = 'Segment count: ' + path.segments.length;
-    };
-
-$scope.mouseUp = function(){
-        var segmentCount = path.segments.length;
-
-		var points = Array();
-
-		// When the mouse is released, simplify it:
-		path.simplify(10);
-	
-		for(i=0; i<path._segments.length; i++)
-		{
-			points.push({'x' : path._segments[i]._point.x, 'y' : path._segments[i]._point.y});	
-		}
-	
-		console.log(points);	
-	
-		var pointsAsString = points.map(function(o){return o.x+","+o.y});
-
-		// Select the path, so we can see its segments:
-		path.fullySelected = true;
-		
-		textItem.content = pointsAsString.join(' ; ');
 	};
 
-/*
 
-
-	var path;
-
-
-    
-
-   
-
-    $scope.mouseDown = function(event){
-    	if (path) {
-			path.selected = false;
-		}
-
-		// Create a new path and set its stroke color to black:
-		path = new paper.Path({
-			segments: [event.point],
-			strokeColor: 'black',
-			// Select the path, so we can see its segment points:
-			fullySelected: true
-		});
-    };
-	paper.install(window);
-    var canvas = document.getElementById('myCanvas');
-    paper.setup('canvas');          
-    */
-	
-	
-
-	/*$http.get('data.json').then( function (response) {
-		console.log(response.data.points);
-		$scope.points=JSON.parse(response.data.points);
-    });*/
 });
--- app/js/directives.js
+++ app/js/directives.js
@@ -1,3 +1,76 @@
 'use strict';
 
-/* Directives */
+app.directive('pgEpicness', function () {
+    return {
+      restrict: 'A',
+      replace: 'false',
+      template: '<canvas resize></canvas>',
+      scope: {
+        points: '=',
+        line: '='
+      },
+      link: function (scope, elems, attrs) {
+      	var canvas = elems[0];
+      	var mainTool = new paper.Tool();
+      	mainTool.activate();
+
+      	paper.setup(canvas);
+
+      	var paperPoints = [];
+      	var paperLine = [];
+
+      	var drawPoints = function(points) {
+      		angular.forEach(points, function(point) {
+      			var path = new paper.Path.Circle(new Point(point.x, point.y), 5);
+            	path.strokeColor = 'gray';
+            	path.strokeWidth = 2;
+      		});
+      	};
+
+      	var selected = null;
+      	var drag = false;
+      	mainTool.onMouseDown = function(event) {
+      		var hit = paper.project.hitTest(event.point)
+      		if (hit) {
+      			selected = hit.item;
+      		} else {
+      			var path = new paper.Path.Circle(new Point(event.point.x, event.point.y), 5);
+            	path.strokeColor = 'gray';
+            	path.strokeWidth = 2;
+            	path.fillColor = 'white';
+            	/*scope.points.push({
+      				x: event.point.x,
+	      			y: event.point.y
+      			});
+      			scope.$apply();*/
+            	selected = path;
+      		};
+
+      		drag = true;
+      	};
+
+      	mainTool.onMouseMove = function(event) {
+      		if (drag && selected) {
+      			selected.position = event.point;
+      			scope.$apply();
+      		}
+      	}
+
+      	mainTool.onMouseUp = function(event) {
+      		drag = false;
+      	}
+
+/*
+        scope.$watchCollection('points', function(newVal) {
+        	console.log("update");
+         	if (newVal) {
+         		paper.project.clear();
+            	drawPoints(scope.points);
+          	}
+        }, true);
+*/
+
+        drawPoints();
+      }
+    }
+});
