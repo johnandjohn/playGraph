ddba3742edbbb85c9d1654ac23b44d8f1f8872ec
Nicolas Bornand <nicolas74@gmail.com>
2014-05-10 17:14:51 +0200

    boostrap modified

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,9 @@
 'use strict';
 
-/* App Module */
+var app = angular.module('playGraph',[]);
+
+var Point = function(x,y){
+    this.x = x;
+    this.y = y;
+};
+
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,9 @@
 'use strict';
 
 /* Controllers */
+var p = new Point(1,2);
+
+app.controller('pointCtrl', function($scope){
+    $scope.song= [
+    ];
+});
