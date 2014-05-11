d559fccdb5dd6eb57a1c0029a677c90408f38c46
Nicolas Bornand <nicolas74@gmail.com>
2014-05-11 13:25:36 +0200

    song label

--- app/js/app.js
+++ app/js/app.js
@@ -10,75 +10,93 @@ var Point = function(x,y){
 var dummyPoints= [
 		{
 			x: 10,
-			y: 20
+			y: 20,
+                        label:"offspring"
 		},
 		{
 			x: 30,
-			y: 30
+			y: 30,
+                        label:"gunther"
 		},
 		{
 			x: 50,
-			y: 25
+			y: 25,
+                        label:"bloodhoung gang"
 		},
 		{
 			x: 60,
-			y: 200
+			y: 200,
+                        label:"linkin park"
 		},
 		{
 			x: 90,
-			y: 230
+			y: 230,
+                        label:"savant"
 		},
 		{
 			x: 400,
-			y: 300
+			y: 300,
+                        label:"tac tac"
 		},
 		{
 			x: 100,
-			y: 220
+			y: 220,
+                        label:"piste x"
 		},
 		{
 			x: 200,
-			y: 20
+			y: 20,
+                        label:"piste y"
 		},
 		{
 			x: 150,
-			y: 450
+			y: 450,
+                        label:"piste z"
 		},
 		{
 			x: 70,
-			y: 80
+			y: 80,
+                        label:"piste a"
 		},
 		{
 			x: 400,
-			y: 80
+			y: 80,
+                        label:"piste b"
 		},
 		{
 			x: 430,
-			y: 90
+			y: 90,
+                        label:"piste c"
 		},
 		{
 			x: 420,
-			y: 65
+			y: 65,
+                        label:"piste d"
 		},
 		{
 			x: 380,
-			y: 320
+			y: 320,
+                        label:"piste e"
 		},
 		{
 			x: 490,
-			y: 340
+			y: 340,
+                        label:"piste f"
 		},
 		{
 			x: 400,
-			y: 400
+			y: 400,
+                        label:"piste f"
 		},
 		{
 			x: 480,
-			y: 360
+			y: 360,
+                        label:"piste f"
 		},
 		{
 			x: 420,
-			y: 490
+			y: 490,
+                        label:"piste f"
 		}
 
 	]
